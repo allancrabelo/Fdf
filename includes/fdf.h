47f69facec5dd75e73e9b38246eb2a12770d5ba4
Zedro <45104292+PedroZappa@users.noreply.github.com>
2025-07-10 17:55:15 +0100

    added: more files.

--- includes/fdf.h
+++ includes/fdf.h
@@ -3,14 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaugusto <aaugusto@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: aaugusto <<aaugusto@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 01:06:06 by aaugusto          #+#    #+#             */
-/*   Updated: 2025/07/10 01:07:25 by aaugusto         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:59:08 by aaugusto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
+# include "libft.h"
+# include "mlx.h"
+# include <fcntl.h>
+# include <unistd.h>
+#include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
 
 #endif
